f11d2c79f6e0b292935314c7ad5b6e0a6632438a
Arnav Atkare <116661119+Vaanar24@users.noreply.github.com>
2024-07-05 20:11:51 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "f99f22cd12msh90418a9f558372ap187c99jsn7a84016f7b68",
+    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5dfc653775050ecadea4f878a6707bb1";
